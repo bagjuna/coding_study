90be8af4478ff60d3754eb38974db39b0699bc8b
bagjuna <juna.park124@gmail.com>
2025-03-28 11:30:47 +0900

    day5

--- src/juna/package1/q0_10.java
+++ src/juna/package1/q0_10.java
@@ -5,18 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 13458번 시험 감독 성공
+ */
 public class q0_10 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         long cnt = N;              // 반드시 long
-        int[] test = new int[N];   //
+        int[] test = new int[N];   // 시험장 학생수의 인덱스
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < N; i++) {
             test[i] = Integer.parseInt(st.nextToken());
         }
+
         st = new StringTokenizer(br.readLine());
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
--- src/juna/package2/q2_0.java
+++ src/juna/package2/q2_0.java
@@ -1,6 +1,73 @@
 package juna.package2;
 
+// 5원과 2원으로 거스름돈 준다
+// cnt = 0;
+
+// 1. N 입력 받는다
+// 2. 5 미만일때
+// 4. 입력 받은 돈 짝수면 cnt += N / 5
+// 5. 입력 받은 돈 홀수면 cnt += N / 5 - 1
+
+
+import java.util.Scanner;
+
+/**
+ * 거스름돈 (백준 14916)
+ */
 public class q2_0 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 0;
+
+        if (N < 5) {
+            if (N == 2 || N == 4) {
+                System.out.println(N / 2);
+            } else {
+                System.out.println(-1);
+            }
+            return;
+        }
+
+        if ((N % 5) == 0) {
+            System.out.println(N / 5);
+            return;
+        }
+
+
+        // 홀수 일때
+        if (N % 2 == 1) {
+            if (N >= 5)
+                N -= 5; // 출력할 때 cnt + 1해서 출력
+            else {
+                System.out.println(-1);
+            }
+            while (N > 0) {
+                N -= 2;
+                cnt += 1;
+            }
+            if (cnt > 5) {
+                cnt = (cnt / 5) * 2 + cnt % 5;
+                System.out.println(cnt + 1);
+            } else {
+                System.out.println(cnt + 1);
+            }
+
+
+        } else {
+            while (N > 0) {
+                N -= 2;
+                cnt += 1;
+            }
+            if (cnt > 5) {
+                cnt = (cnt / 5) * 2 + cnt % 5;
+                System.out.println(cnt);
+            } else
+                System.out.println(cnt);
+        }
+
+
+    }
 
 }
