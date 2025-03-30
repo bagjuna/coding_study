dde940ae79ca48333db66d9329d3444d3405d453
bagjuna <juna.park124@gmail.com>
2025-03-30 13:11:51 +0900

    day6

---
+++ src/juna/package2/q2_13.java
@@ -0,0 +1,36 @@
+package juna.package2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 모두의 마블 (백준 12845)
+ */
+public class q2_13 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int gold = 0;
+        int level = 0;
+        int N = sc.nextInt();
+        List<Integer> cards = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            cards.add(sc.nextInt());
+        }
+        Integer first = cards.stream().max(Comparator.naturalOrder()).orElse(0);
+        cards.remove(first);
+        level = first;
+        for (int i = 0; i < N-1; i++) {
+            Integer max = cards.stream().max(Comparator.naturalOrder()).orElse(0);
+            cards.remove(max);
+            gold += level;
+            gold += max;
+        }
+
+        System.out.println(gold);
+
+
+    }
+}
